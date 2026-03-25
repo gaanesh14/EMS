5f0e419d952b40317dbc524c59b001e1ba8828d2
gaanesh14 <ganeshallu14@gmail.com>
2026-03-25 11:28:39 +0530

    Update CORS configuration to allow multiple origins

--- backend/index.js
+++ backend/index.js
@@ -13,10 +13,11 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: process.env.VITE_API_URL || "http://localhost:5173",
-  // credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: [
+    "http://localhost:5173",
+    "https://ems-frontend-5aq9.onrender.com"
+  ],
+  credentials: true
 }));
 
 app.use(express.json());
